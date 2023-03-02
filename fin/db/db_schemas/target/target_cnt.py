70e7ebad73f041e155cf80a6373386ef0bcd0b81
Beilak <beylak@yandex.ru>
2023-03-02 23:22:35 +0300

    add Target DB migration

---
+++ fin/db/db_schemas/target/target_cnt.py
@@ -0,0 +1,25 @@
+from sqlalchemy import String, Column, ForeignKey, DateTime, func, Numeric, Integer
+from sqlalchemy.orm import composite
+from sqlalchemy_utils.types.currency import CurrencyType
+
+from fin.db.db_conn import Base
+from fin.models.common import Money
+
+
+class TargetCnt(Base):
+    __tablename__ = "target_cnt"
+    target_cnt_id_type = Integer()
+    target_cnt_id = Column(target_cnt_id_type, primary_key=True, autoincrement=True)
+    unit_id = Column(String(32))
+    name = Column(String(32))
+    description = Column(String(128))
+    value = Column(Numeric(precision=8))
+    currency = Column(CurrencyType)
+    init_value = Column(Numeric(precision=8))
+    init_currency = Column(CurrencyType)
+    user_login = Column(String(32))
+    cr_date = Column(DateTime(timezone=True), server_default=func.now())
+    upd_date = Column(DateTime(timezone=True), onupdate=func.now())
+
+    target_money = composite(Money, value, currency)
+    init_money = composite(Money, init_value, init_currency)
