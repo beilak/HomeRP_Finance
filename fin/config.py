b058b6a3db29c9ee419477798a85f1dc8fec3c8e
Beilak <beylak@yandex.ru>
2023-03-08 02:39:36 +0300

    fix compose ports

--- fin/config.py
+++ fin/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     db_user = "fin_test_user"
     db_pwd = "fin_test_pwd"
     db_host = "127.0.0.1"
-    db_port = "5000"
+    db_port = "5001"
     db_name = "fin_test"
 
     auth_url = "http://127.0.0.1:8090"
